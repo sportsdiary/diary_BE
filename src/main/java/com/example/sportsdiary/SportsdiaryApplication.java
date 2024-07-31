9dc7bd6cc3242534b1c76d278740c2e2c7d72bf5
heeone1 <rinakim1019@gmail.com>
2024-07-31 17:43:25 +0900

    초기설정

---
+++ src/main/java/com/example/sportsdiary/SportsdiaryApplication.java
@@ -0,0 +1,13 @@
+package com.example.sportsdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsdiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsdiaryApplication.class, args);
+	}
+
+}
